98cb18e35507c85ca60f2dd89ddbb74d4b7390df
Shumakher Eduard <wendalin@protonmail.ch>
2024-03-04 08:37:30 +0100

    Tree search

---
+++ ex3.py
@@ -0,0 +1,102 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.height = 1
+        self.left = None
+        self.right = None
+
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\\t" * level + prefix + str(self.key) + "\\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
+
+def get_height(node):
+    if not node:
+        return 0
+    return node.height
+
+def get_balance(node):
+    if not node:
+        return 0
+    return get_height(node.left) - get_height(node.right)
+
+def left_rotate(z):
+    y = z.right
+    T2 = y.left
+
+    y.left = z
+    z.right = T2
+
+    z.height = 1 + max(get_height(z.left), get_height(z.right))
+    y.height = 1 + max(get_height(y.left), get_height(y.right))
+
+    return y
+
+def right_rotate(y):
+    x = y.left
+    T3 = x.right
+
+    x.right = y
+    y.left = T3
+
+    y.height = 1 + max(get_height(y.left), get_height(y.right))
+    x.height = 1 + max(get_height(x.left), get_height(x.right))
+
+    return x
+
+def min_value_node(node):
+    current = node
+    while current.left is not None:
+        current = current.left
+    return current
+
+def insert(root, key):
+    if not root:
+        return Node(key)
+
+    if key < root.key:
+        root.left = insert(root.left, key)
+    elif key > root.key:
+        root.right = insert(root.right, key)
+    else:
+        return root
+
+    root.height = 1 + max(get_height(root.left), get_height(root.right))
+
+    balance = get_balance(root)
+
+    if balance > 1:
+        if key < root.left.key:
+            return right_rotate(root)
+        else:
+            root.left = left_rotate(root.left)
+            return right_rotate(root)
+
+    if balance < -1:
+        if key > root.right.key:
+            return left_rotate(root)
+        else:
+            root.right = right_rotate(root.right)
+            return left_rotate(root)
+
+    return root
+def sum_values(node):
+    if node is None:
+        return 0
+    return node.key + sum_values(node.left) + sum_values(node.right)
+
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+root.right.right.right = Node(8)
+
+print("Sum Values:", sum_values(root))
